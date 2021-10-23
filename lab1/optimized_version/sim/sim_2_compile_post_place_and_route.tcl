1d037f343aaded583f87621196efbd6e95fff230
aledolme19 <s285624@studenti.polito.it>
2021-10-23 11:05:07 +0200

    sim_update
    sim

---
+++ lab1/optimized_version/sim/sim_2_compile_post_place_and_route.tcl
@@ -0,0 +1,13 @@
+vcom -93 -work work ../tb/clk_gen.vhd
+vcom -93 -work work ../tb/data_maker.vhd
+vcom -93 -work work ../tb/data_sink.vhd
+vlog -work work ../innovus/IIR_1lookahead.v
+vlog -work work ../tb/tb_iir_1lookahead.v
+
+vsim -L /software/dk/nangate45/verilog/msim6.2g work.tb_iir_1lookahead
+vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_iir/DUT=../innovus/IIR_1lookahead.sdf work.tb_iir_1lookahead
+
+vcd file ../netlist/post_place_and_route/vcd/iir_1lookahead_syn.vcd
+vcd add /tb_iir_1lookahead/DUT/*
+
+run 5 us
