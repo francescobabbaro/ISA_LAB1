2e13a9f038d4bb3269c2a915d9f941f2c6b76c21
francescobabbaro <francesco.babbaro@gmail.com>
2021-11-04 23:11:31 +0100

    First version Innovus redone

--- lab1/first_version/innovus/innovus.cmd
+++ lab1/first_version/innovus/innovus.cmd
@@ -1,7 +1,7 @@
 #######################################################
 #                                                     
 #  Innovus Command Logging File                     
-#  Created on Tue Oct 19 12:47:03 2021                
+#  Created on Thu Nov  4 23:35:17 2021                
 #                                                     
 #######################################################
 
@@ -21,59 +21,15 @@ win
 set init_design_netlisttype verilog
 set init_design_settop 1
 set init_top_cell IIR
-set init_verilog ../netlist/backannotation/IIR.v
+set init_verilog ../netlist/backannotation/synopsys/IIR_no_resynth.v
 set init_lef_file /software/dk/nangate45/lef/NangateOpenCellLibrary.lef
 set init_gnd_net VSS
 set init_pwr_net VDD
 set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_mmmc_file mmm_design.tcl
-init_design
-set init_design_netlisttype verilog
-set init_design_settop 1
-set init_top_cell IIR
-set init_verilog ../netlist/backannotation/iir.v
-set init_lef_file /software/dk/nangate45/lef/NangateOpenCellLibrary.lef
-set init_gnd_net VSS
-set init_pwr_net VDD
 init_design
 getIoFlowFlag
 setIoFlowFlag 0
-floorPlan -coreMarginsBy die -site FreePDK45_38x28_10R_NP_162NW_34O -r 1.0 0.6 5.0 5.0 5.0 5.0
+floorPlan -coreMarginsBy die -site FreePDK45_38x28_10R_NP_162NW_34O -r 1 0.6 5 5 5 5
 uiSetTool select
 getIoFlowFlag
 fit
@@ -97,22 +53,17 @@ set sprCreateIeStripeWidth 10.0
 set sprCreateIeStripeThreshold 1.0
 setAddRingMode -ring_target default -extend_over_row 0 -ignore_rows 0 -avoid_short 0 -skip_crossing_trunks none -stacked_via_top_layer metal10 -stacked_via_bottom_layer metal1 -via_using_exact_crossover_size 1 -orthogonal_only true -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
 addRing -nets {VDD VSS} -type core_rings -follow core -layer {top metal1 bottom metal1 left metal2 right metal2} -width {top 0.8 bottom 0.8 left 0.8 right 0.8} -spacing {top 0.8 bottom 0.8 left 0.8 right 0.8} -offset {top 1.8 bottom 1.8 left 1.8 right 1.8} -center 1 -extend_corner {} -threshold 0 -jog_distance 0 -snap_wire_center_to_grid None
-selectWire 2.9650 2.9200 3.7650 78.5600 2 VDD
 clearGlobalNets
 globalNetConnect VDD -type pgpin -pin VDD -inst * -module {}
 globalNetConnect VSS -type pgpin -pin VSS -inst * -module {}
 setSrouteMode -viaConnectToShape { noshape }
 sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { metal1(1) metal10(10) } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { metal1(1) metal10(10) } -nets { VDD VSS } -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { metal1(1) metal10(10) }
-deselectAll
-selectWire 80.1050 1.3200 80.9050 80.1600 2 VSS
-deselectAll
-selectWire 80.1050 1.3200 80.9050 80.1600 2 VSS
 setPlaceMode -prerouteAsObs {1 2 3 4 5 6 7 8}
+setPlaceMode -prerouteAsObs {1 2 3 4 5 6 7 8}
+setMultiCpuUsage -localCpu 4 -cpuPerRemoteHost 4 -remoteHost 0 -keepLicense true
+setDistributeHost -local
 setPlaceMode -fp false
 placeDesign
-deselectAll
-selectWire 25.5950 51.3800 25.7350 54.4600 4 i_DP_i_MULTIPLIER_B2_mult_28_n51
-gui_select -rect {114.121 52.351 128.872 64.454}
 setOptMode -fixCap true -fixTran true -fixFanoutLoad false
 optDesign -postCTS
 optDesign -postCTS -hold
@@ -127,27 +78,38 @@ setNanoRouteMode -quiet -drouteEndIteration default
 setNanoRouteMode -quiet -routeWithTimingDriven false
 setNanoRouteMode -quiet -routeWithSiDriven false
 routeDesign -globalDetail
-saveDesign IIR_v1_not_opt
 setAnalysisMode -analysisType onChipVariation
 setOptMode -fixCap true -fixTran true -fixFanoutLoad false
 optDesign -postRoute
 optDesign -postRoute -hold
-saveDesign IIR_v1_opt.enc
 getDrawView
 setDrawView fplan
 win
-dumpToGIF ../../../Desktop/ss_IIR.fplan.gif
+dumpToGIF snapshots/ss_after_post_routing.fplan.gif
 getDrawView
 setDrawView amoeba
 win
-dumpToGIF ../../../Desktop/ss_IIR.amoeba.gif
+dumpToGIF snapshots/ss_after_post_routing.amoeba.gif
 getDrawView
 setDrawView place
 win
-dumpToGIF ../../../Desktop/ss_IIR.place.gif
+dumpToGIF snapshots/ss_after_post_routing.place.gif
 checkPlace checkplace.ss.rpt
-gui_select -rect {95.175 28.110 93.851 45.508}
-dumpToGIF ../../../Desktop/IIR_screen_capture
+fit
+getDrawView
+setDrawView fplan
+win
+dumpToGIF snapshots/ss_after_post_routing.fplan.gif
+getDrawView
+setDrawView amoeba
+win
+dumpToGIF snapshots/ss_after_post_routing.amoeba.gif
+getDrawView
+setDrawView place
+win
+dumpToGIF snapshots/ss_after_post_routing.place.gif
+checkPlace checkplace.ss.rpt
+dumpToGIF snapshots/after_post_routing
 reset_parasitics
 extractRC
 rcOut -setload IIR.setload -rc_corner my_rc
@@ -167,4 +129,9 @@ saveNetlist IIR.v
 all_hold_analysis_views 
 all_setup_analysis_views 
 write_sdf  -ideal_clock_network IIR.sdf
-saveDesign IIR_v1_final.enc
+saveDesign IIR.enc
+get_visible_nets
+setDrawView place
+setDrawView fplan
+setDrawView ameba
+setDrawView place
